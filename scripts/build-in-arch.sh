02182d37b9e7668c9a5f4848c6e65464fa54d8c4
emoeem <3353745917@qq.com>
2026-09-21 15:27:32 +0800

    ci: allow shared yay git cache ownership

--- scripts/build-in-arch.sh
+++ scripts/build-in-arch.sh
@@ -54,6 +54,7 @@ if [[ "$prepared_image" == "1" ]]; then
     git config --global --add safe.directory "*"
     git config --global --add safe.directory "*"
     git config --global --add safe.directory "*"
+    git config --global --add safe.directory "*"
     # yay may reuse Git repositories from the shared host cache. Git 2.55+
     # rejects these as dubious ownership when the cache was created by root.
     git config --global --add safe.directory '*'
